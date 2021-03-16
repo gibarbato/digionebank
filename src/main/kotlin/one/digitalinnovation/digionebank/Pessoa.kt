626190e214fefeb5636839c605d8bb1d22df859b
gibarbato <gibarbato@gmail.com>
2021-03-15 22:20:54 -0300

    updated to apply the private codifier to set function of cpf property

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,14 +1,17 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
-    var nome:String = "Almir"
-    var cpf:String = "002.589.951-88"
+    var nome: String = "Almir"
 
+    var cpf: String = "002.589.951-88"
+    private set
 }
+
 
 fun main(){
     val almir = Pessoa()
 
+    println(almir)
     println(almir.nome)
     println(almir.cpf)
-}
+  }
